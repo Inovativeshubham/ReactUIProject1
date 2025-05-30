970bc54610fdbb36d7226ad195a527624c595eec
Inovativeshubham <shubhamrai966666@gmail.com>
2025-05-31 02:12:52 +0530

    changes doing

--- src/addedNewFeature/NewFeature.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NewFeature = () => {
-  return (
-    <div>NewFeature</div>
-  )
-}
-
-export default NewFeature
--- src/version3/version3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const version3 = () => {
-  return (
-    <div>version3</div>
-  )
-}
-
-export default version3
